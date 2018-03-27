b16e5e7f148e25d72f767e0a7a29229089ca056c
leonardocarvalho <leonardo@geekie.com.br>
2018-03-27 01:36:49 -0300

    Encode QRCode base 27

--- js/helpers/index.js
+++ js/helpers/index.js
@@ -9,27 +9,48 @@ const padDigits = (value, numberDigits, filler = '0') => {
   );
 };
 
-const encodeStickers = (stickers) => stickers
-  .sort((s1, s2) => s1.stickerNumber  - s2.stickerNumber)
-  .map(s => s.count > 1 ? 2 : s.count)
-  .reduce((acc, n) => acc.times(3).plus(n), new BigNumber(1))
-  .toString();
+const ENCODING27 = '0ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+
+const encodeStickers = (stickers) => {
+  let add = (3 - stickers.length % 3) % 3;
+  let str = stickers
+    .sort((s1, s2) => s1.stickerNumber  - s2.stickerNumber)
+    .map(s => s.count > 1 ? 2 : s.count)
+  while (add > 0) {
+    str.push(0);
+    add--;
+  }
+
+  let index = 0;
+  let encoded = '';
+  while (index < str.length) {
+    const v = str[index] + str[index + 1] * 3 + str[index + 2] * 9;
+    encoded += ENCODING27[v];
+    index += 3;
+  }
+
+  return encoded;
+};
 
 const decodeStickers = (encoded, size) => {
   const decoded = [];
-  let index = size;
-  let number = new BigNumber(encoded);
-  while (index > 0) {
-    index--;
-    const next = number.modulo(3).toNumber();
-    decoded.push(next);
-    number = number.dividedToIntegerBy(3);
+  for (let index = 0; index < encoded.length; index++) {
+    let charIndex = ENCODING27.indexOf(encoded[index]);
+    if (charIndex === -1) {
+      // to handle error; For now just fill with ones so no exchange
+      while (decoded.length < size) {
+        decoded.push(1);
+      }
+      return decoded;
+    }
+    decoded.push(charIndex % 3);
+    charIndex = parseInt(charIndex / 3);
+    decoded.push(charIndex % 3);
+    charIndex = parseInt(charIndex / 3);
+    decoded.push(charIndex);
   }
 
-  // Number should be exactly 1
-  console.log(number);
-
-  return decoded.reverse();
+  return decoded.slice(0, size);
 };
 
 
